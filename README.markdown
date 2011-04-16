1086f110d35d19d1ebfcba61b0cd6182db76e244
Alan Maciel <alan.maciel@ubernetics.com>
2011-04-15 20:01:53 -0500

    Commit inicial

---
+++ README.markdown
@@ -0,0 +1,4 @@
+#santuario
+This is a project called santuario.
+####[*2011 übernetics Agile Software*](http://ubernetics.com/)
+[more info](http://ubernetics.com/products/santuario)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Santuario::Application.config.secret_token = 'eb96125d2e2bbf836b8b4f7e3f122964e3029286e32bf92e83582722a916e8a8b7d3a2b9d89ae25c9b2078aacbf90f55f93d55e39a897fd34eb7e25ec44e71ba'
